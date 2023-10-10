bcb1d75575484954f28654efdd614d493515a369
smtamme <stellamariitamme@gmail.com>
2023-10-10 14:59:20 +0300

    finished catController

--- backend/controllers/cat.controller.js
+++ backend/controllers/cat.controller.js
@@ -3,18 +3,36 @@
 let cats = ["fluffy", "kitty"]
 
 exports.create = (req, res) => {
-    
+    //POST
+
     const { name } = req.params
-    console.log(name)
+    //console.log(name)
     cats.push(name)
     res.send(name)
 }
 
 exports.read = (req, res) => {
+    //GET
     //console.log(req.user)
     res.send(cats)
 }
 
-exports.update = (req, res) => {}
+exports.update = (req, res) => {
+    //PUT
+    const { name } = req.params
+    const catIndex = cats.indexOf(name)
+    if (catIndex !== -1) {
+        cats[catIndex] = cats[catIndex] + name
+    }
+    res.send(cats)
+}
 
-exports.delete = (req, res) => {}
+exports.delete = (req, res) => {
+    //DELETE
+    const { name } = req.params
+    const catIndex = cats.indexOf(name)
+    if (catIndex !== -1) {
+        cats.splice(catIndex, 1)
+    }
+    res.send(cats)
+}
